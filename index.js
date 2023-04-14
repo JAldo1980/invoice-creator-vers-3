69eff20efb676a5dcd8560acca803589ec716e97
James Alderman <80925002+JAldo1980@users.noreply.github.com>
2023-04-14 19:36:36 +0100

    updated JS

--- index.js
+++ index.js
@@ -1,68 +1,116 @@
 const addInputsBtn = document.getElementById("add-inputs-btn");
-const inputItem = document.getElementById("input-item");
-const inputCurrency = document.getElementById("input-currency");
-const currencySelectBox = document.getElementById("currency-select-box");
+// output variables
 const itemOutput = document.getElementById("item-output");
 const costOutput = document.getElementById("cost-output");
-const currencyOutput = document.getElementById("currency-output");
 const noteOutput = document.getElementById("note-output");
 const totalOutput = document.getElementById("total-output");
 
-const itemArray = [];
-const costArray = [];
+// constructor function
+function itemConstructor(item, currency, amount, quantity) {
+  this.item = item;
+  this.currency = currency;
+  this.amount = amount;
+  this.quantity = quantity;
+  this.id = generateRandomID();
+}
 
+// now, I have create an array to store the objects
+const items = [];
+
+// EVENT LISTENER
 addInputsBtn.addEventListener("click", function () {
-  // check if item and cost already exist in arrays before adding
-  if (itemArray.indexOf(inputItem.value) === -1) {
-    itemArray.push(inputItem.value);
-  }
-  //   CONVERT costArray strings to numbers - will help with sum total later on
-  if (costArray.indexOf(Number(inputCurrency.value)) === -1) {
-    costArray.push(Number(inputCurrency.value));
-  }
-  // render item
-  itemOutput.innerHTML = "";
-  itemArray.forEach(function (item) {
-    itemOutput.innerHTML += `
-    <div class="item-flex">
-    <p>${item}</p>
-    <p class="remove-text">remove</p>
-    </div>
+  // here, I collect the input values...
+  const item = document.querySelector("#input-item").value;
+  const currency = document.querySelector("#currency-select-box").value;
+  const amount = document.querySelector("#input-currency").value;
+  const quantity = document.querySelector("#input-quantity").value;
+
+  // now, I create a new object using the above constructor function - pushing the new object to the array...
+  const newItem = new itemConstructor(item, currency, amount, quantity);
+  items.push(newItem);
+  console.log(newItem);
+  console.log("items array", items);
+
+  let totalUnitCosts = 0;
+  items.forEach(function (item) {
+    totalUnitCosts += item.amount * item.quantity;
+  });
+
+  // now I render the inputs to the specified html elements
+  itemOutput.innerHTML = items
+    .map(function (item) {
+      const totalItemCost = item.amount * item.quantity;
+      return `
+        <div class="item-flex" id="${item.id}">
+          <div>${item.item}</div> 
+          <div class="qty-text">${item.quantity}</div>
+          <div class="item-cost-text">${item.currency} ${item.amount}</div>
+          <div>${item.currency} ${totalItemCost}</div>
+        </div>`;
+    })
+    .join("");
+
+  // once input has been rendered, I need to CLEAR out the input fields
+  document.getElementById("input-item").value = "";
+  document.getElementById("input-currency").value = "";
+  document.getElementById("input-quantity").value = "";
+
+  // calculate the TOTAL AMOUNT
+  let totalAmount = 0;
+  items.forEach(function (item) {
+    totalAmount += item.amount * item.quantity;
+  });
+
+  totalOutput.innerHTML = `
+    <div class="total-amount-el">${totalAmount}</div>
   `;
-  });
-  //   render cost
-  costOutput.innerHTML = "";
-  costArray.forEach(function (cost) {
-    costOutput.innerHTML += `
-    <p>${cost}</p>
-    `;
-  });
-  // RENDER note output
-  document.getElementById("note-output").innerHTML = `
-   <p>We accept cash, credit card or PayPal.</p>
-   `;
-  // reset input values
-  inputItem.value = "";
-  inputCurrency.value = "";
 
-  //   CALCULATE TOTAL COSTS
-
-  let totalCost = 0;
-  costArray.forEach(function (cost) {
-    //   the below ensures a string is not returned
-    totalCost += Number(cost);
-  });
-
-  console.log(costArray);
-
-  // RENDER TOTAL COSTS
-  document.getElementById("total-output").innerHTML = `
-       <p class="adjust-class"> ${totalCost}</p>
-       `;
+  // render note
+  noteOutput.innerHTML = `
+    <p>We accept cash, credit card, or PayPal</p>
+  `;
 });
+// END OF EVENT LISTENER
 
-// REMOVE text functionality
+// function to generate random ID
+function generateRandomID() {
+  let id = "";
+  const characters = "0123456789abcdef";
 
-itemOutput.addEventListener("click", function (e) {
-  console.log(e);
-});
+  for (let i = 0; i < 32; i++) {
+    id += characters[Math.floor(Math.random() * characters.length)];
+  }
+
+  return id;
+}
+
+// MODAL SECTION
+
+mockText.innerHTML = `
+<!-- user inputs 1 -->
+<div class="user-details-container">
+  <div id="invoice-number">
+    <!-- rendered by javascript -->
+    <p>Invoice Number: 001</p>
+  </div>
+  <div id="issue-date">
+    <!-- rendered by javascript -->
+    <p>Issue Date: 20/04/23</p>
+  </div>
+  <div id="due-date">
+    <!-- rendered by javascript -->
+    <p><strong>Due Date: 14/05/23</strong></p>
+  </div>
+</div>
+<!-- user inputs 2 -->
+<div class="user-details-container">
+  <div id="sender-address">
+    <!-- rendered by javascript -->
+    <p>Senders Address</p>
+  </div>
+  <div id="recipient-address">
+    <!-- rendered by javascript -->
+    <p>Recipient Address</p>
+  </div>
+</div>
+`;
